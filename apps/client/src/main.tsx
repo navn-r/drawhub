c4e293aea9e4efd0a93d777c0ca70f8713377d9e
Navinn Ravindaran <navinn.ravindaran@mail.utoronto.ca>
2022-06-30 18:43:20 -0400

    add client scaffolding

---
+++ apps/client/src/main.tsx
@@ -0,0 +1,19 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './app/app';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <StrictMode>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </StrictMode>
+);
