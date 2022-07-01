b0dc8ff04bac11c5727f65ef0fe991e27fd990b6
Navinn Ravindaran <navinn.ravindaran@mail.utoronto.ca>
2022-07-01 11:18:22 -0400

    add mongodb connection setup

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { ServerAuthModule } from '@drawhub/server/auth';
 
 @Module({
-  imports: [ServerAuthModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule.forRoot({ envFilePath: `.env` })],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ServerAuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
