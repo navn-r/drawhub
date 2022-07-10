775c74bf19ce01f8c975faf36795d4f9a7feeb4b
Navinn Ravindaran <navinn.ravindaran@mail.utoronto.ca>
2022-07-09 21:47:54 -0400

    add create canvas button

--- libs/client/home/dashboard/src/lib/client-home-dashboard.tsx
+++ libs/client/home/dashboard/src/lib/client-home-dashboard.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, Flex, Heading } from '@chakra-ui/react';
-import { FaPlus } from 'react-icons/fa';
+import { Box, Flex, Heading } from '@chakra-ui/react';
+import CreateCanvasButton from './create-canvas-button/create-canvas-button';
 
 /* eslint-disable-next-line */
 export interface ClientHomeDashboardProps {}
@@ -9,13 +9,7 @@ export function ClientHomeDashboard(props: ClientHomeDashboardProps) {
     <Box w={'100%'}>
       <Flex justifyContent={'space-between'} mb={5}>
         <Heading>Dashboard</Heading>
-        <Button
-          leftIcon={<FaPlus />}
-          bgGradient={'linear-gradient(90deg, rgba(131, 58, 180, .9) 0%, rgba(253, 29, 29, .9) 100%)'}
-          colorScheme={'light'}
-        >
-          New
-        </Button>
+        <CreateCanvasButton />
       </Flex>
       <Heading size={'xs'} as={'h5'} fontWeight={600} color={'gray.600'}>
         MY CANVASSES
