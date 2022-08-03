5f57d026d23876a80ff4e5ba2cbfbb212f5dfbf8
Navinn Ravindaran <navinn@duck.com>
2022-08-03 10:19:48 -0400

    hotfix(email): fix `undefined` canvas name

--- libs/server/email/src/lib/email.consumer.ts
+++ libs/server/email/src/lib/email.consumer.ts
@@ -11,13 +11,13 @@ export class EmailConsumer {
 
   @Process('send-stitch')
   async sendEmail(job: Job) {
-    this.logger.log(`[${job.data.name}] SENDING STITCH EMAIL <${job.data.owner}>`);
+    this.logger.log(`[${job.data.canvasId}] SENDING STITCH EMAIL <${job.data.owner}>`);
 
     await this.mailService.sendMail({
       to: job?.data.owner,
       from: 'drawhubnoreply@gmail.com',
-      subject: `Your canvas ${job.data.name} has been Stitched!`,
-      text: `Hey ${job.data.owner}!\n Your canvas ${job.data.name} has been stitched by ${job.data.email}!`,
+      subject: `Your canvas ${job.data.canvasName} has been Stitched!`,
+      text: `Hey ${job.data.owner}!\n Your canvas ${job.data.canvasName} has been stitched by ${job.data.email}!`,
     });
 
     this.logger.log(`[${job.data.canvasId}] SENT TO <${job.data.owner}>`);
@@ -33,9 +33,7 @@ export class EmailConsumer {
       subject: `You have been invited!`,
       html: `
         <p>
-          Hey, ${job?.data.email}! You have been invited to <strong>${job?.data.canvasName}</strong> by ${job.data.owner}!
-          <br/>
-          <a href="http://localhost/home/draw/${job?.data.canvasId}">Click Here</a> to get drawing!
+          Hey, ${job?.data.email}! You have been invited to <strong>${job?.data.canvasName}</strong> by ${job.data.owner}! <a href="https://drawhub.tech/home/draw/${job?.data.canvasId}">Click Here</a> to get drawing!
           <br/>
           - DrawHub Team
         <p>
